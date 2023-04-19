5a477db6e8b84335473cba9d6a0ba45a95595fb6
Jonathan Yazdanpanah <75292585+jyazdan@users.noreply.github.com>
2023-04-19 18:50:50 -0400

    finished project

---
+++ src/NoteLayout.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Note } from './App'
+import { Navigate, Outlet, useOutletContext, useParams } from 'react-router-dom'
+
+type NoteLayoutProps = {
+  notes: Note[]
+}
+
+const NoteLayout = ({notes}: NoteLayoutProps) => {
+  const { id } = useParams()
+  const note = notes.find(n => n.id === id)
+
+  if (note == null) return <Navigate to="/" replace />
+
+  return <Outlet context={note} />
+}
+
+export function useNote(){
+  return useOutletContext<Note>()
+}
+
+export default NoteLayout
